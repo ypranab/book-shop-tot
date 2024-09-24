b2efa79f38396520ca7b832d823a3fd2cbdd154c
ypranab11 <ypranab11@gmail.com>
2024-09-24 21:10:13 +0600

    book card updated

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -3,16 +3,27 @@ import { Link } from "react-router-dom";
 
 const Books = ({ book }) => {
   const bookState = book;
+  const { bookId, bookName, tags, author, category, rating } = book;
 
   return (
-    <Link to={`/book/${book?.bookId}`} state={bookState}>
+    <Link to={`/book/${bookId}`} state={bookState}>
       <div className="card bg-base-100 w-96 h-96 shadow-xl">
         <figure className="px-10 pt-10">
           <img src={`${book?.image}`} alt="Books" />
         </figure>
         <div className="card-body">
-          <h2 className="card-title">{book?.bookName}</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <h2 className="card-title">{bookName}</h2>
+          <p>Author : {author}</p>
+          <p>Category: {category}</p>
+          <p>Rating: {rating}</p>
+          <p>
+            Tags:{" "}
+            {tags.map((tag, idx) => (
+              <span key={idx} tag={tag}>
+                {"#" + tag + " "}
+              </span>
+            ))}
+          </p>
           <div className="card-actions justify-end">
             <button className="btn btn-primary">Book Details</button>
           </div>
