190b465301e7914444d46e90ec9080e31d06fc70
ypranab11 <ypranab11@gmail.com>
2024-09-24 21:18:32 +0600

    book details updated

--- src/pages/SingleBookDetails.jsx
+++ src/pages/SingleBookDetails.jsx
@@ -14,17 +14,36 @@ const SingleBookDetails = () => {
   }, []);
 
   const singleBook = allBooks.find((book) => book.bookId == book_id);
+  const {
+    bookName,
+    image,
+    rating,
+    category,
+    review,
+    tags,
+    totalPages,
+    publisher,
+    yearOfPublishing,
+  } = singleBook;
   console.log(singleBook);
 
   return (
     <div className="card lg:card-side bg-base-100 shadow-xl w-3/4 mx-auto m-10 py-16">
-      <figure className="w-1/2">
-        <img src={`${singleBook?.image}`} alt="Album" />
+      <figure className="w-fit">
+        <img src={`${image}`} alt="Album" />
       </figure>
       <div className="card-body">
-        <h2 className="card-title">{singleBook?.bookName}</h2>
-        <p>{author}</p>
+        <h2 className="card-title">{bookName}</h2>
+        <p>Author: {author}</p>
+        <p>Review: {review}</p>
+        <p>Category: {category}</p>
+        <p>Rating: {rating}</p>
+        <p>Total Pages: {totalPages}</p>
+        <p>Tags: {tags}</p>
+        <p>Publisher: {publisher}</p>
+        <p>Year Of Publishing: {yearOfPublishing}</p>
         <div className="card-actions justify-end">
+          <button className="btn btn-primary">Wish to Read</button>
           <button className="btn btn-primary">Add to Cart</button>
         </div>
       </div>
